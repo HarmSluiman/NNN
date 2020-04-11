457b35d7188a28cb823143aad1361b6811858d16
harmsluiman <harm.sluiman@gmail.com>
2020-04-10 21:30:20 -0400

    impl for signal decay at bouton

--- NNNImpl/src/nnn/nerve/Bouton.java
+++ NNNImpl/src/nnn/nerve/Bouton.java
@@ -1,12 +1,56 @@
 package nnn.nerve;
 
+
+
 public class Bouton {
 
-	private float potential = 0;
+	private double potential = 0;
+	private Thread decayThread = new Thread(() -> {decayPotential(this);});
 	
-	public void stimulate(float signal)
+	public void stimulate(double signal)
 	{
 		potential = potential + signal;
+		decay();
 	};
+	
+	private void setPotential(double value)
+	{
+		potential = value;
+	};
+	
+	private double getPotential()
+	{
+		return potential;
+	};
+	
+	/*
+	 * decay 
+	 * 
+	 * the decay method
+	 */
+	private void decay()
+	{
+		decayThread.start();
+	};
+	
+	private void decayPotential(Bouton bouton)
+	{
+		while (bouton.getPotential() > .001) {
+			
+			try
+			{
+			    Thread.sleep(1000);
+			}
+			catch(InterruptedException ex)
+			{
+			    Thread.currentThread().interrupt();
+			}
+			
+			bouton.setPotential(bouton.getPotential() * .9);
+		}
+		
+		
+	};
+	
 	
 }
