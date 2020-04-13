321d8e415fc5793c74f3357e3472b51977b4ecfa
harmsluiman <harm.sluiman@gmail.com>
2020-04-13 11:23:36 -0400

    adding initial Nerve code

--- NNNImpl/src/nnn/nerve/Nerve.java
+++ NNNImpl/src/nnn/nerve/Nerve.java
@@ -2,9 +2,25 @@ package nnn.nerve;
 
 public class Nerve {
 
+	Dendrite[] dendrites = {new Dendrite()};
+	
+	Axon axon = new Axon();
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	public Nerve() {
+		
+	}
+	
+	private void propagateSignal() {
+		// accumulate the input stimulations 
+		
+	}
+	
+	public void atrohy() {
+		
 	}
 
 }
